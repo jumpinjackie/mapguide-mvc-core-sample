9854b6edb749aee9a774dea63149b589ab81e05c
Jackie Ng <jumpinjackie@gmail.com>
2016-01-13 03:08:15 +1100

    Use platform-specific json configuration files to store the MapGuide web config path.
    Also update MapGuide dependency to match the expected package name.

--- src/Sheboygan/Startup.cs
+++ src/Sheboygan/Startup.cs
@@ -1,17 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNet.Builder;
+﻿using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Hosting;
-using Microsoft.AspNet.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using OSGeo.MapGuide;
+using System;
+using System.Runtime.InteropServices;
 
 namespace Sheboygan
 {
     public class Startup
     {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder();
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                builder.AddJsonFile("appsettings.windows.json");
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                builder.AddJsonFile("appsettings.linux.json");
+            }
+            else
+            {
+                throw new NotSupportedException("MapGuide doesn't work on this platform");
+            }
+
+            // Set up configuration sources.
+            builder.AddEnvironmentVariables();
+            Configuration = builder.Build();
+
+            string mgWebConfigPath = Configuration["MapGuide.WebConfigPath"];
+            MapGuideApi.MgInitializeWebTier(mgWebConfigPath);
+        }
+
+        public IConfigurationRoot Configuration { get; set; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -22,7 +46,6 @@ namespace Sheboygan
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app)
         {
-            MapGuideApi.MgInitializeWebTier("C:\\Program Files\\OSGeo\\MapGuide\\Web\\www\\webconfig.ini");
             app.UseStaticFiles();
             app.UseMvcWithDefaultRoute();
         }
