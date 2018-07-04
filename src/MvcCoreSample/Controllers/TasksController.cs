c7bb45e5899e5302d92235fd39b3a49f6b84a17f
Jackie Ng <jumpinjackie@gmail.com>
2018-07-05 00:40:01 +1000

    #2: Implement "Hello Map" and "Hello Viewer" sections of devguide

--- src/MvcCoreSample/Controllers/TasksController.cs
+++ src/MvcCoreSample/Controllers/TasksController.cs
@@ -1,10 +1,27 @@
 using Microsoft.AspNetCore.Mvc;
 using MvcCoreSample.Models;
+using OSGeo.MapGuide;
 
 namespace MvcCoreSample.Controllers
 {
     public class TasksController : MgBaseController
     {
         public IActionResult Home(MapGuideCommandModel model) => View(model);
+
+        public IActionResult HelloMap(MapGuideCommandModel model) => View(model);
+
+        public IActionResult GotoPoint(GotoPointModel model) => View(model);
+
+        public IActionResult HelloViewer(MapGuideCommandModel model) => View(model);
+
+        public IActionResult DisplaySpatialReference(MapGuideCommandModel model)
+        {
+            var conn = this.CreateConnection(model);
+            var map = new MgMap(conn);
+            map.Open(model.MapName);
+            
+            ViewData["SpatialReference"] = map.GetMapSRS();
+            return View(model);
+        }
     }
 }
