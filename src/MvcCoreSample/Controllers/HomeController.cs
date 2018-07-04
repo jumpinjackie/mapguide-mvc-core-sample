fdf79eda0961d653318707f9563b2a7f08d4494e
Jackie Ng <jumpinjackie@gmail.com>
2018-07-05 01:12:12 +1000

    #2: Allow refresh to work by reusing the current session id

--- src/MvcCoreSample/Controllers/HomeController.cs
+++ src/MvcCoreSample/Controllers/HomeController.cs
@@ -31,41 +31,55 @@ namespace MvcCoreSample.Controllers
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
 
-        public IActionResult Main()
+        public IActionResult Main(string session)
         {
             var conn = new MgSiteConnection();
-            var userInfo = new MgUserInformation("Anonymous", "");
-            conn.Open(userInfo);
-            var site = conn.GetSite();
-            var sessionId = site.CreateSession();
-            //Important: Attach the generated session id, otherwise we can't save to any resources in the session repo
-            userInfo.SetMgSessionId(sessionId);
-            var resSvc = (MgResourceService)conn.CreateService(MgServiceType.ResourceService);
-
-            //mapguide-react-layout makes an incorrect assumption during init that if it was initialized with a session id, then
-            //a runtime map must've already been created as it assumes it's coming back from a browser refresh, so to cater for
-            //this assumption, pre-create the runtime map state and save it.
-            var map = new MgMap(conn);
+            string sessionId = session;
+            MgResourceService resSvc = null;
             var mdfId = new MgResourceIdentifier("Library://Samples/Sheboygan/Maps/Sheboygan.MapDefinition");
-            var mapName = mdfId.Name;
-            map.Create(mdfId, mapName);
-            var mapStateId = new MgResourceIdentifier($"Session:{sessionId}//{mapName}.Map");
-            var sel = new MgSelection(map);
-            sel.Save(resSvc, mapName);
-            map.Save(resSvc, mapStateId);
+            if (string.IsNullOrEmpty(sessionId))
+            {
+                var userInfo = new MgUserInformation("Anonymous", "");
+                conn.Open(userInfo);
+                var site = conn.GetSite();
+                sessionId = site.CreateSession();
+                //Important: Attach the generated session id, otherwise we can't save to any resources in the session repo
+                userInfo.SetMgSessionId(sessionId);
+                resSvc = (MgResourceService)conn.CreateService(MgServiceType.ResourceService);
 
-            var path = Path.Combine(_hostEnv.ContentRootPath, "Data/WebLayout.xml");
-            var content = new StringBuilder(System.IO.File.ReadAllText(path))
-                .Replace("$RESOURCE_ID", mdfId.ToString())
-                .Replace("$HOME_URL", Url.Action(nameof(SamplesController.Home), "Samples", null, Url.ActionContext.HttpContext.Request.Scheme));
-
-            var bytes = Encoding.UTF8.GetBytes(content.ToString());
-            var bs = new MgByteSource(bytes, bytes.Length);
-            var br = bs.GetReader();
+                //mapguide-react-layout makes an incorrect assumption during init that if it was initialized with a session id, then
+                //a runtime map must've already been created as it assumes it's coming back from a browser refresh, so to cater for
+                //this assumption, pre-create the runtime map state and save it.
+                var map = new MgMap(conn);
+                var mapName = mdfId.Name;
+                map.Create(mdfId, mapName);
+                var mapStateId = new MgResourceIdentifier($"Session:{sessionId}//{mapName}.Map");
+                var sel = new MgSelection(map);
+                sel.Save(resSvc, mapName);
+                map.Save(resSvc, mapStateId);
+            }
+            else //Reuse existing session
+            {
+                var userInfo = new MgUserInformation(sessionId);
+                conn.Open(userInfo);
+                resSvc = (MgResourceService)conn.CreateService(MgServiceType.ResourceService);
+            }
 
             var resId = new MgResourceIdentifier($"Session:{sessionId}//Sheboygan.WebLayout");
-            resSvc.SetResource(resId, br, null);
+            //This should already exist if refreshing, otherwise ...
+            if (!resSvc.ResourceExists(resId))
+            {
+                var path = Path.Combine(_hostEnv.ContentRootPath, "Data/WebLayout.xml");
+                var content = new StringBuilder(System.IO.File.ReadAllText(path))
+                    .Replace("$RESOURCE_ID", mdfId.ToString())
+                    .Replace("$HOME_URL", Url.Action(nameof(SamplesController.Home), "Samples", null, Url.ActionContext.HttpContext.Request.Scheme));
 
+                var bytes = Encoding.UTF8.GetBytes(content.ToString());
+                var bs = new MgByteSource(bytes, bytes.Length);
+                var br = bs.GetReader();
+                
+                resSvc.SetResource(resId, br, null);
+            }
             var model = new ViewerModel
             {
                 LayoutId = resId.ToString(),
